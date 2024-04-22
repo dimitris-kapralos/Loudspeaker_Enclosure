59ce7a915dd7db555450f68a9b06ef34a24827a2
Dimitris Kapralos <mitsoskprl@gmail.com>
2024-04-22 17:56:13 +0300

    adding variables and changing the way plots are displayed

--- Dodecahedron_Closed_Box.py
+++ Dodecahedron_Closed_Box.py
@@ -20,25 +20,100 @@ lsp_parameters = {
     "height": 0.09,
 }
 
+clb_parameters0 = {
+    "number_of_speakers": 1,
+    "lx": 0.17,
+    "ly": 0.1,
+    "lz": 0.19,
+    "x": 0.075,
+    "y1": 0.15,
+    "y2": 0.15,
+    "r" : 0.13,
+    "d" : 0.064,
+    "Wmax": 100,
+    "truncation_limit" : 10  
+}
+
 clb_parameters1 = {
     "number_of_speakers": 1,
-    "lx": 0.25,
-    "ly": 0.65,
+    "lx": 0.11,
+    "ly": 0.2,
     "lz": 0.19,
+    "x": 0.075,
+    "y1": 0.15,
+    "y2": 0.15,
     "r" : 0.15,
     "d" : 0.064,
     "Wmax": 100,
     "truncation_limit" : 10  
 }
 
-dod_parameters = {  
+clb_parameters2 = {
+    "number_of_speakers": 1,
+    "lx": 0.175,
+    "ly": 0.2,
+    "lz": 0.19,
+    "x": 0.075,
+    "y1": 0.15,
+    "y2": 0.15,
+    "r" : 0.18,
+    "d" : 0.064,
+    "Wmax": 100,
+    "truncation_limit" : 10  
+}
+
+clb_parameters3 = {
+    "number_of_speakers": 1,
+    "lx": 0.2,
+    "ly": 0.22,
+    "lz": 0.19,
+    "x": 0.075,
+    "y1": 0.15,
+    "y2": 0.15,
+    "r" : 0.21,
+    "d" : 0.064,
+    "Wmax": 100,
+    "truncation_limit" : 10  
+}
+
+clb_parameters4 = {
+    "number_of_speakers": 1,
+    "lx": 0.25,
+    "ly": 0.22,
+    "lz": 0.19,
+    "x": 0.075,
+    "y1": 0.15,
+    "y2": 0.15,
+    "r" : 0.23,
+    "d" : 0.064,
+    "Wmax": 100,
+    "truncation_limit" : 10  
+}
+
+dod_parameters0 = {
+    "Vab": 7.64,
+}
+
+dod_parameters1 = {  
     "Vab": 11.4,           
                   
 }
 
+dod_parameters2 = {
+    "Vab": 22.28,
+}
+
+dod_parameters3 = {
+    "Vab": 31.72,
+}
+
+dod_parameters4 = { 
+    "Vab": 43.52,
+}
+
 octave_steps = 24
 frequencies_per_octave = octave_steps * np.log(2)
-min_frequency = 10
+min_frequency = 1
 max_frequency = 10000
 num_points = int(octave_steps * np.log2(max_frequency / min_frequency)) + 1
 frequencies = np.logspace(
@@ -46,15 +121,26 @@ frequencies = np.logspace(
 )
 
 # Create instance of Loudspeaker and ClosedBoxEnclosure
-enclosure1 = DodecahedronEnclosure(clb_parameters1, dod_parameters ,lsp_parameters)
+enclosure0 = DodecahedronEnclosure(clb_parameters0, dod_parameters0 ,lsp_parameters)
+enclosure1 = DodecahedronEnclosure(clb_parameters1, dod_parameters1 ,lsp_parameters)
+enclosure2 = DodecahedronEnclosure(clb_parameters2, dod_parameters2 ,lsp_parameters)
+enclosure3 = DodecahedronEnclosure(clb_parameters3, dod_parameters3 ,lsp_parameters)
+enclosure4 = DodecahedronEnclosure(clb_parameters4, dod_parameters4 ,lsp_parameters)
 
 # Calculate the system response
-response, impedance, power, spl = enclosure1.calculate_dodecahedron_response(frequencies)
-
+response0, impedance0, power0, spl0 = enclosure0.calculate_dodecahedron_response(frequencies, 3 , 0.33)
+response, impedance, power, spl = enclosure1.calculate_dodecahedron_response(frequencies, 3, 0.36)
+response2, impedance2, power2, spl2 = enclosure2.calculate_dodecahedron_response(frequencies, 3, 0.42)
+response3, impedance3, power3, spl3 = enclosure3.calculate_dodecahedron_response(frequencies, 3, 0.45)
+response4, impedance4, power4, spl4 = enclosure4.calculate_dodecahedron_response(frequencies, 3, 0.5)
 
 # Plot the system response
 fig1, ax1 = plt.subplots()
-ax1.semilogx(frequencies, response, label='1 loudspeaker')
+ax1.semilogx(frequencies, response0, label='V = 7.64 L')
+ax1.semilogx(frequencies, response, label='V = 11.4 L')
+ax1.semilogx(frequencies, response2, label='V = 22.28 L')
+ax1.semilogx(frequencies, response3, label='V = 31.72 L')
+ax1.semilogx(frequencies, response4, label='V = 43.52 L')
 ax1.set_xlabel('Frequency (Hz)')
 ax1.set_ylabel('dB rel. Uref')
 ax1.set_title('System Response')
@@ -64,17 +150,26 @@ plt.show()
 
 # Plot the impedance
 fig2, ax2 = plt.subplots()
-ax2.semilogx(frequencies, impedance, label='1 loudspeaker')
+ax2.semilogx(frequencies, impedance0, label='V = 7.64 L')
+ax2.semilogx(frequencies, impedance, label='V = 11.4 L')
+ax2.semilogx(frequencies, impedance2, label='V = 22.28 L')
+ax2.semilogx(frequencies, impedance3, label='V = 31.72 L')
+ax2.semilogx(frequencies, impedance4, label='V = 43.52 L')
 ax2.set_xlabel('Frequency (Hz)')
 ax2.set_ylabel('Impedance (Ohms)')
 ax2.set_title('System Impedance')
 ax2.grid(which='both')
+ax2.set_ylim(0, 50)
 ax2.legend()
 plt.show()
 
 # Plot the power
 fig3, ax3 = plt.subplots()
-ax3.semilogx(frequencies, power, label='1 loudspeaker')
+ax3.semilogx(frequencies, power0, label='V = 7.64 L')
+ax3.semilogx(frequencies, power, label='V = 11.4 L')
+ax3.semilogx(frequencies, power2, label='V = 22.28 L')
+ax3.semilogx(frequencies, power3, label='V = 31.72 L')
+ax3.semilogx(frequencies, power4, label='V = 43.52 L')
 ax3.set_xlabel('Frequency (Hz)')
 ax3.set_ylabel('dB rel. 1pW')
 ax3.set_title("Sound Power Lw")
@@ -84,12 +179,15 @@ plt.show()
 
 
 # plot the sound power level
-fig4, ax4 = plt.subplots()
-ax4.semilogx(frequencies, spl, label='1 loudspeaker')
-ax4.set_xlabel('Frequency (Hz)')
-ax4.set_ylabel('dB rel. 20uPa')
-ax4.set_title("Sound Pressure Lw")
-ax4.grid(which='both')
-ax4.legend()
-
+# fig4, ax4 = plt.subplots()
+# ax4.semilogx(frequencies, spl, label='Vm = Va')
+# ax4.semilogx(frequencies, spl2, label='Vm = Va/2')
+# ax4.semilogx(frequencies, spl3, label='Vm = Va/3')
+# ax4.semilogx(frequencies, spl4, label='Vm = Va/4')
+# ax4.set_xlabel('Frequency (Hz)')
+# ax4.set_ylabel('dB rel. 20uPa')
+# ax4.set_title("Sound Pressure Level (SPL)")
+# ax4.grid(which='both')
+# ax4.legend()
+# plt.show()
 
