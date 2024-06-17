650a8303389617d6660833cb3d8d40307ae76687
Dimitris Kapralos <mitsoskprl@gmail.com>
2024-06-17 16:22:35 +0300

    parameters for CLI.py

---
+++ parameters.py
@@ -0,0 +1,470 @@
+# Description: This file contains the data of the loudspeakers used in the project.
+list_of_loudspeakers = [
+    {
+        "name": "Visaton W100S-4 Ohm",
+        "Re": 3.4,
+        "Le": 0.0005,
+        "e_g": 1.83,
+        "Qes": 0.54,
+        "Qms": 3.04,
+        "fs": 37,
+        "Sd": 0.0129,
+        "Vas": 4.49,
+        "Qts": 0.46,
+        "Cms": 0.001127,
+        "Mms": 0.053,
+        "Bl": 3.7,
+        "radius": 0.035,
+        "height": 0.048,
+        "r": 0.05,
+    },
+    {
+        "name": "Dayton Audio PC105 -4 Ohm",
+        "Re": 3.3,
+        "Le": 0.00032,
+        "e_g": 1.82,
+        "Qes": 0.67,
+        "Qms": 2.17,
+        "fs": 81.4,
+        "Sd": 0.0053,
+        "Vas": 3.68,
+        "Qts": 0.67,
+        "Cms": 0.00092,
+        "Mms": 0.0042,
+        "Bl": 3.3,
+        "radius": 0.035,
+        "height": 0.044,
+        "r": 0.065,
+    },    
+    {
+        "name": "Dayton Audio DSA135-8 Ohm",
+        "Re": 5.9,
+        "Le": 0.00078,
+        "e_g": 2.43,
+        "Qes": 0.47,
+        "Qms": 2.04,
+        "fs": 51.8,
+        "Sd": 0.0075,
+        "Vas": 7.93,
+        "Qts": 0.38,
+        "Cms": 0.00099,
+        "Mms": 0.0095,
+        "Bl": 6.22,
+        "radius": 0.05,
+        "height": 0.045,
+        "r": 0.05,
+    },
+    {
+        "name": "Visaton FR 10 - 8 Ohm",
+        "Re": 7.2,
+        "Le": 0.0002,
+        "e_g": 2.68,
+        "Qes": 0.95,
+        "Qms": 2.29,
+        "fs": 90,
+        "Sd": 0.005,
+        "Vas": 2.29,
+        "Qts": 0.67,
+        "Cms": 0.000648,
+        "Mms": 0.0057,
+        "Bl": 4.6,
+        "radius": 0.035,
+        "height": 0.044,
+        "r": 0.065,
+    },
+    {
+        "name": "Peerless by Tymphany 830656 5-1/4- 8 Ohm",
+        "Re": 6.1,
+        "Le": 0.00037,
+        "e_g": 2.47,
+        "Qes": 0.7,
+        "Qms": 3.6,
+        "fs": 57,
+        "Sd": 0.0087,
+        "Vas": 8.78,
+        "Qts": 0.59,
+        "Cms": 0.00083,
+        "Mms": 0.0093,
+        "Bl": 5.36,
+        "radius": 0.035,
+        "height": 0.058,
+        "r": 0.066,
+    },
+    {
+        "name": "Epique E150HE-44 5-1/2 - 8 Ohm",
+        "Re": 6.6,
+        "Le": 0.0016,
+        "e_g": 2.6,
+        "Qes": 0.45,
+        "Qms": 2.94,
+        "fs": 40,
+        "Sd": 0.0095,
+        "Vas": 7.65,
+        "Qts": 0.39,
+        "Cms": 0.0006,
+        "Mms": 0.026,
+        "Bl": 4.6,
+        "radius": 0.06,
+        "height": 0.055,
+        "r": 0.069,
+    },
+    {
+        "name": "Tang Band W5-1138SMF - 4 Ohm",
+        "Re": 3.4,
+        "Le": 0.00034,
+        "e_g": 1.84,
+        "Qes": 0.57,
+        "Qms": 3.56,
+        "fs": 45,
+        "Sd": 0.0094,
+        "Vas": 4.8,
+        "Qts": 0.49,
+        "Cms": 0.00037,
+        "Mms": 0.025,
+        "Bl": 7.17,
+        "radius": 0.05,
+        "height": 0.081,
+        "r": 0.065,
+    },
+    {
+        "name": "Visaton SC 13 - 8 Ohm",
+        "Re": 7.2,
+        "Le": 0.0007,
+        "e_g": 2.68,
+        "Qes": 0.94,
+        "Qms": 2.95,
+        "fs": 78,
+        "Sd": 0.0085,
+        "Vas": 7.7,
+        "Qts": 0.71,
+        "Cms": 0.00163,
+        "Mms": 0.0079,
+        "Bl": 4.2,
+        "radius": 0.033,
+        "height": 0.051,
+        "r": 0.065,
+    },
+    {
+        "name": "VISATON WF 130 ND - 8 Ohm  ",
+        "Re": 6.7,
+        "Le": 0.00076,
+        "e_g": 2.58,
+        "Qes": 0.89,
+        "Qms": 2.97,
+        "fs": 46,
+        "Sd": 0.0094,
+        "Vas": 10.6,
+        "Qts": 0.69,
+        "Cms": 0.000846,
+        "Mms": 0.012,
+        "Bl": 5.39,
+        "radius": 0.044,
+        "height": 0.019,
+        "r": 0.065,
+    },                   
+    {
+        "name": "Visaton W130s - 4 Ohm",
+        "Re": 3.5,
+        "Le": 0.00061,
+        "e_g": 1.87,
+        "Qes": 0.55,
+        "Qms": 2.54,
+        "fs": 52,
+        "Sd": 0.0074,
+        "Vas": 14,
+        "Qts": 0.45,    
+        "Cms": 0.0014,
+        "Mms": 0.0054,
+        "Bl": 3.6,
+        "radius": 0.035,
+        "height": 0.053,
+        "r" : 0.065
+    },
+        {
+        "name": "FE126NV2-8 Ohm  ",
+        "Re": 7.1,
+        "Le": 0.000038,
+        "e_g": 2.66,
+        "Qes": 0.37,
+        "Qms": 3.51,
+        "fs": 79,
+        "Sd": 0.0055,
+        "Vas": 8.5,
+        "Qts": 0.34,
+        "Cms": 0.0014,
+        "Mms": 0.03,
+        "Bl": 5.2,
+        "radius": 0.045,
+        "height": 0.057,
+        "r": 0.058,
+    },         
+    {
+        "name": "Dayton Audio ND140-8 Ohm  ",
+        "Re": 7.8,
+        "Le": 0.00144,
+        "e_g": 2.8,
+        "Qes": 0.79,
+        "Qms": 5.01,
+        "fs": 56,
+        "Sd": 0.0087,
+        "Vas": 7.74,
+        "Qts": 0.68,
+        "Cms": 0.00076,
+        "Mms": 0.01,
+        "Bl": 6.05,
+        "radius": 0.045,
+        "height": 0.055,
+        "r": 0.057,
+    },   
+        {
+        "name": "Fostex FF 125 WK-8 Ohm  ",
+        "Re": 6.7,
+        "Le": 0.000044,
+        "e_g": 2.58,
+        "Qes": 0.464,
+        "Qms": 5.091,
+        "fs": 67,
+        "Sd": 0.0066,
+        "Vas": 7.265,
+        "Qts": 0.42,
+        "Cms": 0.001153,
+        "Mms": 0.05,
+        "Bl": 5.59,
+        "radius": 0.045,
+        "height": 0.057,
+        "r": 0.06,
+    }, 
+]
+
+clb_par = {
+    "number_of_speakers": 1,
+    "lx": 0.25,
+    "ly": 0.22,
+    "lz": 0.19,
+    "x": 0.075,
+    "y1": 0.15,
+    "y2": 0.15,
+    "r": 0.18,
+    "d": 0.064,
+    "Wmax": 100,
+    "truncation_limit": 10,
+}
+
+
+pentagon_edges_icosi = [
+    {"edge": 0.1, "type": "icosidodecahedron", "rd": 0.137},
+    {"edge": 0.11, "type": "icosidodecahedron", "rd": 0.151},
+    {"edge": 0.12, "type": "icosidodecahedron", "rd": 0.165},
+    {"edge": 0.13, "type": "icosidodecahedron", "rd": 0.178},
+    {"edge": 0.14, "type": "icosidodecahedron", "rd": 0.192},
+    {"edge": 0.15, "type": "icosidodecahedron", "rd": 0.206},
+    {"edge": 0.16, "type": "icosidodecahedron", "rd": 0.22},
+    {"edge": 0.17, "type": "icosidodecahedron", "rd": 0.234},
+    {"edge": 0.18, "type": "icosidodecahedron", "rd": 0.247},
+]
+
+pentagon_edges_dodeca  = [
+    {"edge": 0.1, "type":"dodecahedron", "rd": 0.111},
+    {"edge": 0.11, "type":"dodecahedron", "rd": 0.122},
+    {"edge": 0.12, "type":"dodecahedron", "rd": 0.13},
+    {"edge": 0.13, "type":"dodecahedron", "rd": 0.134},
+    {"edge": 0.14, "type":"dodecahedron", "rd": 0.156},
+    {"edge": 0.15, "type":"dodecahedron", "rd": 0.167},
+    {"edge": 0.16, "type":"dodecahedron", "rd": 0.178},
+    {"edge": 0.17, "type":"dodecahedron", "rd": 0.19},
+    {"edge": 0.18, "type":"dodecahedron", "rd": 0.20},
+]
+
+port_parameters = [
+    {"t": 0.023, "radius": 0.0079, "length": "0.018"},
+    {"t": 0.0247, "radius": 0.0105, "length": "0.018"},
+    {"t": 0.0265, "radius": 0.0133, "length": "0.018"},
+    {"t": 0.0292, "radius": 0.0175, "length": "0.018"},
+    {"t": 0.0311, "radius": 0.0205, "length": "0.018"},
+    {"t": 0.03575, "radius": 0.0079, "length": "0.023"},
+    {"t": 0.0480, "radius": 0.0105, "length": "0.023"},
+    {"t": 0.0456, "radius": 0.0133, "length": "0.023"},
+    {"t": 0.0525, "radius": 0.0175, "length": "0.023"},
+    {"t": 0.0578, "radius": 0.0205, "length": "0.023"},
+    {"t": 0.04075, "radius": 0.0079, "length": "0.028"},
+    {"t": 0.0458, "radius": 0.0105, "length": "0.028"},
+    {"t": 0.0506, "radius": 0.0133, "length": "0.028"},
+    {"t": 0.0577, "radius": 0.0175, "length": "0.028"},
+    {"t": 0.0628, "radius": 0.0205, "length": "0.028"},
+    {"t": 0.0464, "radius": 0.0079, "length": "0.033"},
+    {"t": 0.0508, "radius": 0.0105, "length": "0.033"},
+    {"t": 0.0556, "radius": 0.0133, "length": "0.033"},
+    {"t": 0.0627, "radius": 0.0175, "length": "0.033"},
+    {"t": 0.0678, "radius": 0.0205, "length": "0.033"},
+    {"t": 0.0514, "radius": 0.0079, "length": "0.038"},
+    {"t": 0.0558, "radius": 0.0105, "length": "0.038"},
+    {"t": 0.0606, "radius": 0.0133, "length": "0.038"},
+    {"t": 0.0677, "radius": 0.0175, "length": "0.038"},
+    {"t": 0.0728, "radius": 0.0205, "length": "0.038"},
+]
+number_of_ports = [ 4, 8, 12, 16, 20]
+
+empty_list_of_port_parameters = {
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # Placeholder function for Optimization menu
+# def optimization_menu():
+#     while True:
+#         clear_screen()
+#         print("Optimization Menu:")
+#         print("1. Select optimization parameters")
+#         print("0. Back to main menu")
+
+#         choice = input("Enter your choice: ").strip()
+
+#         if choice == '1':
+#             sources = ["Dodecahedron Closed Box", "Icosidodecahedron Closed Box", "Icosidodecahedron Bass Reflex", "Another Box Type"]
+#             algorithms = ["Algorithm 1", "Algorithm 2", "Algorithm 3", "Algorithm 4"]
+
+#             # Selecting source
+#             while True:
+#                 clear_screen()
+#                 print("\nSelect a source for the optimization:")
+#                 for idx, source in enumerate(sources, 1):
+#                     print(f"{idx}. {source}")
+#                 print("0. Go back")
+#                 source_choice = input("Enter the number of your choice: ").strip()
+
+#                 if source_choice == '0':
+#                     break
+#                 elif source_choice.isdigit():
+#                     source_choice = int(source_choice) - 1
+
+#                     if source_choice < 0 or source_choice >= len(sources):
+#                         print("Invalid choice. Please enter a number within the range.")
+#                     else:
+#                         selected_source = sources[source_choice]
+#                         print(f"\nYou selected: {selected_source}")
+
+#                         # Selecting loudspeakers
+#                         while True:
+#                             clear_screen()
+#                             print("\nAvailable loudspeakers:")
+#                             for idx, speaker in enumerate(loudspeakers, 1):
+#                                 print(f"{idx}. {speaker['name']}")
+#                             print("0. Go back")
+#                             speaker_choice = input("\nEnter the numbers of the loudspeakers you want to select, separated by spaces: ").strip()
+
+#                             if speaker_choice == '0':
+#                                 break
+#                             else:
+#                                 speaker_indices = [int(index) - 1 for index in speaker_choice.split() if index.isdigit()]
+#                                 selected_speakers = [loudspeakers[index] for index in speaker_indices]
+
+#                                 if not selected_speakers:
+#                                     print("No loudspeakers selected. Please select at least one.")
+#                                 else:
+#                                     clear_screen()
+#                                     print("\nSelected loudspeakers:")
+#                                     for speaker in selected_speakers:
+#                                         print(f"- {speaker['name']}")
+
+#                                     # Selecting pentagon edges
+#                                     if selected_source == "Dodecahedron Closed Box":
+#                                         edges = pentagon_edges_dodeca
+#                                     elif selected_source in ["Icosidodecahedron Closed Box", "Icosidodecahedron Bass Reflex"]:
+#                                         edges = pentagon_edges_icosi
+#                                     else:
+#                                         edges = []
+
+#                                     if edges:
+#                                         while True:
+#                                             clear_screen()
+#                                             print("\nAvailable pentagon edges:")
+#                                             for idx, edge in enumerate(edges, 1):
+#                                                 print(f"{idx}. Edge: {edge['edge']} Type: {edge['type']} RD: {edge['rd']}")
+#                                             print("b. Go back")
+#                                             edge_choice = input("\nEnter the numbers of the pentagon edges you want to select, separated by spaces: ").strip()
+
+#                                             if edge_choice == '0':
+#                                                 break
+#                                             else:
+#                                                 edge_indices = [int(index) - 1 for index in edge_choice.split() if index.isdigit()]
+#                                                 selected_edges = [edges[index] for index in edge_indices]
+
+#                                                 if not selected_edges:
+#                                                     print("No edges selected. Please select at least one.")
+#                                                 else:
+#                                                     clear_screen()
+#                                                     print("\nSelected pentagon edges:")
+#                                                     for edge in selected_edges:
+#                                                         print(f"- Edge: {edge['edge']} Type: {edge['type']} RD: {edge['rd']}")
+#                                                     input("\nPress Enter to continue...")
+#                                                     break
+#                                         if selected_source == "Icosidodecahedron Bass Reflex":
+#                                             # Selecting port parameters
+#                                             while True:
+#                                                 clear_screen()
+#                                                 print("\nAvailable port parameters:")
+#                                                 for idx, port in enumerate(port_parameters, 1):
+#                                                     print(f"{idx}. T: {port['t']} Radius: {port['radius']}")
+#                                                 print("b. Go back")
+#                                                 port_choice = input("\nEnter the numbers of the port parameters you want to select, separated by spaces: ").strip()
+
+#                                                 if port_choice == '0':
+#                                                     break
+#                                                 else:
+#                                                     port_indices = [int(index) - 1 for index in port_choice.split() if index.isdigit()]
+#                                                     selected_ports = [port_parameters[index] for index in port_indices]
+
+#                                                     if not selected_ports:
+#                                                         print("No ports selected. Please select at least one.")
+#                                                     else:
+#                                                         clear_screen()
+#                                                         print("\nSelected port parameters:")
+#                                                         for port in selected_ports:
+#                                                             print(f"- T: {port['t']} Radius: {port['radius']}")
+#                                                         input("\nPress Enter to continue...")
+#                                                         break        
+#                                     else:
+#                                         input("\nPress Enter to continue...")
+#                                         break
+#                         else:
+#                             continue
+
+#                 else:
+#                     print("Invalid choice. Please enter a number or 'b' to go back.")
+
+#         elif choice == '0':
+#             break  # Exit the loop and go back to the main menu
+#         else:
+#             print("Invalid choice. Please enter 1 or 0.")
